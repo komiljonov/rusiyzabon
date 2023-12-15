2a6f078165201f92d8db1a370ff34fc1fb67fee4
komiljonov <komiljonovshukurullokh@gmail.com>
2023-12-15 22:37:56 +0500

    dfg

--- bot/management/commands/bot.py
+++ bot/management/commands/bot.py
@@ -1,7 +1,7 @@
 import os
 from typing import Any
 from django.core.management.base import BaseCommand
-
+from tg_bot import Bot
 
 
 
@@ -16,5 +16,7 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     def handle(self, *args: Any, **options: Any) -> str | None:
         TOKEN = os.getenv("TOKEN")
+        bot = Bot(TOKEN)
 
-        
+        bot.app.run_polling()
+
